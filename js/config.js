4276183a992962cfb9ad71903169790c37c88db7
Supacek <noskinek@gmail.com>
2026-09-02 09:32:41 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -11,7 +11,7 @@ const CONFIG = {
         { url: "DK.jpg", type: "image", duration: 15 },
         { url: "huslesvarec.jpg", type: "image", duration: 15 },
         { url: "kolegwide4.jpg", type: "image", duration: 10 },
-        { url: "losovani.jpg", type: "image", duration: 15 },
+        { url: "losovani1.jpg", type: "image", duration: 15 },
         { url: "hotelwide.jpg", type: "image", duration: 10 }
     ],
 
