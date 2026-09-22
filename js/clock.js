5a60f0a66b937b11c9a8e8b093304df17cfb0688
Supacek <noskinek@gmail.com>
2026-09-22 11:52:34 +0200

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -3,9 +3,11 @@ function updateClock() {
 
     document.getElementById("clock").textContent =
         now.toLocaleTimeString("cs-CZ", {
+            timeZone: "Europe/Prague",   // český čas (letní i zimní automaticky)
             hour: "2-digit",
             minute: "2-digit",
-            second: "2-digit"
+            second: "2-digit",
+            hour12: false
         });
 }
 
