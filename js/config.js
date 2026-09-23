9672b1f7adf9df6344d0b2bcf8402bfd2bb48ac1
Supacek <noskinek@gmail.com>
2026-09-23 09:44:06 +0200

    Add new slide to slideshow configuration

--- js/config.js
+++ js/config.js
@@ -8,6 +8,7 @@ const CONFIG = {
     // Slideshow
     // =========================
     slides: [
+        { url: "vysledky.jpg", type: "image", duration: 15 },
         { url: "DK.jpg", type: "image", duration: 15 },
         { url: "huslesvarec.jpg", type: "image", duration: 15 },
         { url: "kolegwide4.jpg", type: "image", duration: 10 },
